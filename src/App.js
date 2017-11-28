0223a0273aabdd096f267c90c6a6b2b53b39fd44
Simbidion <simonadwyer@yahoo.com.au>
2017-11-28 15:46:21 +1100

    split todo list and changed to emoji buttons

--- src/App.js
+++ src/App.js
@@ -2,26 +2,112 @@ import React, { Component } from 'react';
 import TodoItem from './components/TodoItem';
 import './App.css';
 
-// Data
-const items = [
-  { description: 'First', completed: true},
-  { description: 'Second', completed: true},
-  { description: 'Third', completed: false}
-]
-
-// UI
 class App extends Component {
+  // Data
+  state = {
+    items: [
+      { description: 'First', completed: true},
+      { description: 'Second', completed: true},
+      { description: 'Third', completed: false},
+      { description: 'Fourth', completed: true}
+    ]
+  }
+  
+  onToggleItemAtIndex = (index) => {
+    this.setState((prevState) => {
+      // Get items
+      const beforeItems = prevState.items
+
+      const afterItems = beforeItems.map((item, currentIndex) => {
+        // When found index of item to change
+        if (currentIndex === index) {
+          return {
+            ...item, 
+            completed: !item.completed
+          }
+        }
+        else {
+          return item
+        }
+      })
+      return {
+        items: afterItems
+      }
+    })
+  }
+
+  // UI
   render() {
+    const items = this.state.items
+    const total = items.length
+
+    let totalCompleted = 0
+    let totalIncomplete = 0
+    items.forEach((item) => {
+      if (item.completed) {
+        totalCompleted += 1
+      }
+      else {
+        totalIncomplete += 1
+      }
+    })
+
+
     return (
       <div className="App">
+        <dl>
+          <dt>Total</dt>
+          <dd>{ total }</dd>
+          <dt>Total completed</dt>
+          <dd>{ totalCompleted }</dd>
+          <dt>Total incomplete</dt>
+          <dd>{ totalIncomplete }</dd>
+        </dl>
+        <h3>Completed items</h3>
         {
-          items.map((item, index) => (
-            <TodoItem 
-              key={ index }
-              description={ item.description }
-              completed={ item.completed }
-            />
-          ))
+          items.map((item, index) => {
+            if (!item.completed) {
+              return null
+            }
+            else {
+              return (
+                <TodoItem 
+                  key={ index }
+                  description={ item.description }
+                  completed={ item.completed }
+                  onToggleCompleted={
+                    () => {
+                      console.log('TodoItem onToggleCompleted recieved', index)
+                      this.onToggleItemAtIndex(index)
+                    }
+                  }
+                />
+              )
+            }
+          })
+        }
+        <h3>Incomplete items</h3>
+        {
+          items.map((item, index) => {
+            if (item.completed) {
+              return null
+            }
+            else {
+              return (
+                <TodoItem 
+                  key={ index }
+                  description={ item.description }
+                  completed={ item.completed }
+                  onToggleCompleted={
+                    () => {
+                      console.log('TodoItem onToggleCompleted recieved', index)
+                      this.onToggleItemAtIndex(index)
+                    }
+                  }
+                />
+              )
+            }
+          })
         }
       </div>
     );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,13 +2,34 @@ import React from 'react'
 
 function TodoItem ({
   description,
-  completed = false // If not passed, default to false
+  completed = false, // If not passed, default to false
+  onToggleCompleted
 }) {
+  let status
+
+  if (completed) {
+    status = '✅'
+  }
+  else {
+    status = '❎'
+  }
+  
   return (
+    <div>
     <label>
-      <input type='checkbox' defaultChecked={ completed } />
-        { description }
+      <button type="button"
+        onClick={
+          (event) => { // Event listener
+            console.log('Clicked button', description)
+            onToggleCompleted()
+          }
+        }>
+        { status }
+      </button> 
+      { description }
     </label>
+    
+    </div>
   )
 }
 
